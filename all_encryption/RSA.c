1802773220b290a92010554c8e199e0cee4e36ae
D1dou <90036715+D1dou@users.noreply.github.com>
2023-04-02 11:12:36 +0200

    RSA ok - manque entrée utilisateur

--- all_encryption/RSA.c
+++ all_encryption/RSA.c
@@ -1,112 +1,34 @@
-#include <openssl/rsa.h>
-#include <openssl/pem.h>
-#include <openssl/err.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <gmp.h>
 
-#define KEY_LENGTH  2048
-#define PUB_EXP     3
-#define PRINT_KEYS
-#define WRITE_TO_FILE
-
-void RSA(char *message, int mode)
+int main(void)
 {
-    size_t pri_len;            // Length of private key
-    size_t pub_len;            // Length of public key
-    char   *pri_key;           // Private key
-    char   *pub_key;           // Public key
-    char   msg[KEY_LENGTH/8];  // Message to encrypt
-    char   *encrypt = NULL;    // Encrypted message
-    char   *decrypt = NULL;    // Decrypted message
-    char   *err;               // Buffer for any error messages
+    mpz_t n, d, e, pt, ct;
 
-    // Generate key pair
-    printf("Generating RSA (%d bits) keypair...\n", KEY_LENGTH);
-    fflush(stdout);
-    RSA *keypair = RSA_generate_key(KEY_LENGTH, PUB_EXP, NULL, NULL);
+    mpz_init(pt);
+    mpz_init(ct);
+    mpz_init_set_str(n, "9516311845790656153499716760847001433441357", 10);
+    mpz_init_set_str(e, "65537", 10);
+    mpz_init_set_str(d, "5617843187844953170308463622230283376298685", 10);
 
-    // To get the C-string PEM form:
-    BIO *pri = BIO_new(BIO_s_mem());
-    BIO *pub = BIO_new(BIO_s_mem());
+    const char *plaintext = "Rawane la boss";
+    mpz_import(pt, strlen(plaintext), 1, 1, 0, 0, plaintext);
 
-    PEM_write_bio_RSAPrivateKey(pri, keypair, NULL, NULL, 0, NULL, NULL);
-    PEM_write_bio_RSAPublicKey(pub, keypair);
+    if (mpz_cmp(pt, n) > 0)
+        abort();
 
-    pri_len = BIO_pending(pri);
-    pub_len = BIO_pending(pub);
+    mpz_powm(ct, pt, e, n);
+    gmp_printf("Encoded:   %Zd\n", ct);
 
-    pri_key = malloc(pri_len + 1);
-    pub_key = malloc(pub_len + 1);
+    mpz_powm(pt, ct, d, n);
+    gmp_printf("Decoded:   %Zd\n", pt);
 
-    BIO_read(pri, pri_key, pri_len);
-    BIO_read(pub, pub_key, pub_len);
+    char buffer[64];
+    mpz_export(buffer, NULL, 1, 1, 0, 0, pt);
+    printf("As String: %s\n", buffer);
 
-    pri_key[pri_len] = '\0';
-    pub_key[pub_len] = '\0';
-
-    #ifdef PRINT_KEYS
-        FILE *out1 = fopen("public_key", "w");
-        fwrite(pub_key, sizeof(*pub_key), pub_len, out1);
-        fclose(out1);
-
-        out1 = fopen("private_key", "w");
-        fwrite(pri_key, sizeof(*pri_key), pri_len, out1);
-        fclose(out1);
-    #endif
-
-    // Get the message to encrypt
-    printf("Message to encrypt:\n");
-    fgets(msg, KEY_LENGTH-1, stdin);
-    msg[strlen(msg)-1] = '\0';
-
-    // Encrypt the message
-    encrypt = malloc(RSA_size(keypair));
-    int encrypt_len;
-    err = malloc(130);
-    if((encrypt_len = RSA_public_encrypt(strlen(msg)+1, (unsigned char*)msg, (unsigned char*)encrypt,
-                    keypair, RSA_PKCS1_OAEP_PADDING)) == -1)
-    {
-        ERR_load_crypto_strings();
-        ERR_error_string(ERR_get_error(), err);
-        fprintf(stderr, "Error encrypting message: %s\n", err);
-        goto free_stuff;
-    }
-
-    #ifdef WRITE_TO_FILE
-        // Write the encrypted message to a file
-        FILE *out = fopen("out.txt", "w");
-        fwrite(encrypt, sizeof(*encrypt),  RSA_size(keypair), out);
-        fclose(out);
-        printf("Encrypted message written to file.\n");
-        free(encrypt);
-        encrypt = NULL;
-
-        printf("Reading the encrypted message and attempting decryption...\n");
-        encrypt = malloc(RSA_size(keypair));
-        out = fopen("out.txt", "r");
-        fread(encrypt, sizeof(*encrypt), RSA_size(keypair), out);
-        fclose(out);
-    #endif
-
-    // Decrypt it
-    decrypt = malloc(encrypt_len);
-    if(RSA_private_decrypt(encrypt_len, (unsigned char*)encrypt, (unsigned char*)decrypt,
-                keypair, RSA_PKCS1_OAEP_PADDING) == -1) 
-    {
-        ERR_load_crypto_strings();
-        ERR_error_string(ERR_get_error(), err);
-        fprintf(stderr, "Error decrypting message: %s\n", err);
-        goto free_stuff;
-    }
-    printf("Decrypted message: %s\n", decrypt);
-
-    free_stuff:
-    RSA_free(keypair);
-    BIO_free_all(pub);
-    BIO_free_all(pri);
-    free(pri_key);
-    free(pub_key);
-    free(encrypt);
-    free(decrypt);
-    free(err);
+    mpz_clears(pt, ct, n, e, d, NULL);
+    return 0;
 }
